dc11c9553c1023b259891aba6419de87ec780ea5
tom-turner <tom.turner@live.co.uk>
2022-04-21 21:02:55 +0100

    Update Header.js

--- client/src/components/FileExplorer/Header.js
+++ client/src/components/FileExplorer/Header.js
@@ -15,9 +15,8 @@ let SearchBar = ({className}) => {
 let Profile = () =>{
 	const { logout } = useContext(AuthenticationContext)
 	return (
-		</div>
-
 		<button onClick={logout} className="relative flex rounded-lg h-full group overflow-hidden bg-gray-200 shadow-md rounded-full justify-center items-center">
+			<div className="absolute z-10 top-5 flex w-16 h-20 bg-gray-500 group-hover:bg-indigo-500 animate-[spin_8s_linear_infinite] blur"></div>
 			<div className="absolute z-0 left-15 flex w-16 h-20 bg-gray-400 group-hover:bg-indigo-400 animate-[spin_20s_linear_infinite] blur"></div>
 			<Avatar className="z-40 fill-gray-800 group-hover:fill-white w-12 h-12" />
 		</button>
