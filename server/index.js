13713fcf8712242aa3305697d6c93bdeb1a6f350
tom-turner <tom.turner@live.co.uk>
2022-04-23 01:17:48 +0100

    fix: cors

--- server/index.js
+++ server/index.js
@@ -9,17 +9,12 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
 
-app.use((req,res,next)=>{
-  let origin = req.headers.origin
-  res.set('Access-Control-Allow-Origin', origin)
-  res.set('Access-Control-Allow-Credentials', true )
-  res.set('Access-Control-Allow-Headers', 'authorization,content-type' )
-  res.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE' )
 
-  next()
-})
 app.use(morgan())
-
+app.use(cors({
+  origin: (origin, next) => next(null, origin),
+  credentials: true
+}))
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(express.urlencoded());
