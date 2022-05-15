1f2fbac089fb74909956fd2d439fe2f64d989953
tom-turner <tom.turner@live.co.uk>
2022-05-15 12:49:20 +0100

    fix: downloads and preview on mobile

--- server/routes/get-file-content.js
+++ server/routes/get-file-content.js
@@ -1,8 +1,12 @@
 const { Files } = require('../../models')
+let fs = require('fs');
 
 module.exports = async (req,res) => {
   console.log('get file content')
   const file = await Files.findBy({ id: req.params.id, user_id: res.locals.user.id })
-  res.set('Content-Disposition', `attachment; filename="${file.user_file_name}"`)
-  res.download( __dirname + `/../../file_storage/${file.checksum}`)
+  var src = __dirname + `/../../file_storage/${file.checksum}`;
+
+  res.setHeader('Content-disposition', 'attachment; filename=' + file.user_file_name);
+  res.setHeader('Content-type', file.file_type);
+  res.download(src, file.user_file_name)
 }
